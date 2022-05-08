f2c8e718a6f49f29291c2a9f43e9e28dffffedd1
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-05-07 23:35:25 -0400

    Finished compilation script (for now)

---
+++ Text Files/README.txt
@@ -0,0 +1,2 @@
+All scripts must have a relative URL (make sure it starts with "./" or simply the directory/file name)
+Make as many scripts defered as possible
--- compile.py
+++ compile.py
@@ -1,25 +1,109 @@
+# ----- settings -----
+nodeCompileDir = "./resources_compiled"
+pythonCompileDir = "./html_compiled"
+production = True
+removeFiles = ["template.html"]
+# Delete intermediate compilation step (resources_compiled folder from node)
+clean_up = True
+
+
+
+# ----- script start -----
 import os
+import shutil
+import subprocess
 
 # Set working directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # Install any node dependencies
-print("Installing dependencies...")
-os.system("npm i firebase html-minifier")
-os.system("npm i -D babel-loader @babel/core @babel/preset-env webpack css-loader \
-           mini-css-extract-plugin postcss-loader postcss-preset-env sass sass-loader \
-           terser-webpack-plugin webpack-cli webpack-remove-empty-scripts webpack")
+print("Checking and installing dependencies...")
+localInstall = "firebase html-minifier"
+devLocalInstall = "babel-loader @babel/core @babel/preset-env webpack css-loader \
+                mini-css-extract-plugin postcss-loader postcss-preset-env sass sass-loader \
+                terser-webpack-plugin webpack-cli webpack-remove-empty-scripts webpack"
+output = len(subprocess.check_output("npm list", stderr=subprocess.STDOUT, shell=True).decode("utf-8").split("\n"))
+if output != len(localInstall.split()) + len(devLocalInstall.split()) + 2:
+    os.system("npm i " + localInstall)
+    os.system("npm i -D " + devLocalInstall)
+
+print("Cleaning compiled resources folder...")
+shutil.rmtree(nodeCompileDir, ignore_errors=True)
 
 # Run compiler
 print("Compiling CSS and JS...")
-os.system("node compiler_resources/compiler_controller.js -y")
+compileMode = "production" if production else "development"
+os.system("node compiler_scripts/compiler_controller.js -py " + nodeCompileDir + " " + compileMode)
 
 # Remove empty output from webpack
 os.rmdir("dist")
 
 print("CSS and JS resources compiled.")
 
+from flask import render_template, Flask
+from glob import glob
+from bs4 import BeautifulSoup, element
+
+print("Cleaning compiled html folder...")
+shutil.rmtree(pythonCompileDir, ignore_errors=True)
+
 print("Compiling HTML...")
-print("All resources injected.")
-print("Minifying HTML...")
+app = Flask(__name__, template_folder=nodeCompileDir)
+with app.app_context():
+    for htmlFile in glob(nodeCompileDir + "/**/*.html", recursive=True):
+        filePathOut = pythonCompileDir + htmlFile[len(nodeCompileDir):]
+        [folderPathOut, fileName] = os.path.split(filePathOut)
+        folderPathIn = os.path.dirname(nodeCompileDir + htmlFile[len(nodeCompileDir):])
+
+        # Get compiled HTML
+        renderedHTML = render_template(htmlFile[len(nodeCompileDir)+1:])
+
+        # Inject resources
+        soup = BeautifulSoup(renderedHTML, features="html.parser")
+
+        stylesheets = soup.findAll("link", {"rel": "stylesheet"})
+        for s in stylesheets:
+            t = soup.new_tag('style')
+            c = element.NavigableString(open(folderPathIn + "/" + s["href"]).read())
+            t.insert(0,c)
+            s.replaceWith(t)
+
+        scripts = soup.findAll("script", {"src": True})
+        for s in scripts:
+            t = soup.new_tag('script')
+            c = element.NavigableString(open(folderPathIn + "/" + s["src"]).read())
+            t.insert(0,c)
+            t['type'] = 'module'
+            s.replaceWith(t)
+        renderedHTML = str(soup)
+
+        # Push compiled HTML to pipeline
+        pipeline = open("html_pipeline.txt", "w+")
+        pipeline.seek(0)
+        pipeline.truncate()
+        pipeline.write(renderedHTML)
+        pipeline.close()
+
+        # Minimize HTML, pass pipeline path
+        if production:
+            os.system("node compiler_scripts/minify_html.js html_pipeline.txt")
+            pipeline = open("html_pipeline.txt", "r")
+            renderedHTML = pipeline.read()
+            pipeline.close()
+
+        # Save to "./compiled_html"
+        os.makedirs(folderPathOut, exist_ok=True)
+        compiledFile = open(filePathOut, "w+")
+        compiledFile.write(renderedHTML)
+        compiledFile.close()
+print("HTML minified and all resources injected.")
+
+for file in removeFiles:
+    if os.path.exists(pythonCompileDir + "/" + file):
+        os.remove(pythonCompileDir + "/" + file)
+
+if clean_up:
+    shutil.rmtree(nodeCompileDir, ignore_errors=True)
+
+print("Files deleted as per user settings.")
 print("Compilation finished!")

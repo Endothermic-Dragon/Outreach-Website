95d8ad25c68096cf38f473bb92811e8f5bf1ee56
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-05-17 17:50:06 -0400

    Fixed server-side google login

--- route.js
+++ route.js
@@ -1,6 +1,39 @@
+const { google } = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+    "672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com",
+    "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",
+    "http://localhost"
+);
+
+const peopleAPI = google.people({
+    version: 'v1',
+    auth: oauth2Client
+})
+
 const express = require('express')
 const app = express()
 const port = 80
+
+app.use(express.json());
+
+app.post("/validate-login-code", async function(req, res){
+    if (req.get("X-Requested-With") == "javascript-fetch"){
+        let tokenResponse = await oauth2Client.getToken(req.body.code)
+
+        oauth2Client.setCredentials(tokenResponse.tokens)
+
+        let response = await peopleAPI.people.get({
+            resourceName: "people/me",
+            personFields: "names,photos,emailAddresses"
+        })
+
+        let data = response.data
+
+        console.log(data)
+        res.status(200).send({data: data})
+    }
+})
 
 app.use("/static", express.static('build/static'))
 app.use("/style", express.static('build/style'))
--- src/static/google-sign-in.js
+++ src/static/google-sign-in.js
@@ -1,20 +1,49 @@
 function signIn(){
-  const client = google.accounts.oauth2.initTokenClient({
+  // const client = google.accounts.oauth2.initTokenClient({
+  //   client_id: '672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com',
+  //   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+  //   callback: (tokenResponse) => {
+  //     console.log(tokenResponse)
+      
+  //     fetch('https://people.googleapis.com/v1/people/me?personFields=names,photos,emailAddresses', {
+  //       "method": "GET",
+  //       "headers": {
+  //         "Authorization": "Bearer " + tokenResponse.access_token
+  //       }
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  //   },
+  // });
+
+  // client.requestAccessToken();
+
+  const client = google.accounts.oauth2.initCodeClient({
     client_id: '672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
-    callback: (tokenResponse) => {
-      console.log(tokenResponse)
-      
-      fetch('https://people.googleapis.com/v1/people/me?personFields=names,photos,emailAddresses', {
-        "method": "GET",
-        "headers": {
-          "Authorization": "Bearer " + tokenResponse.access_token
+    ux_mode: 'popup',
+    callback: (response) => {
+      console.log(response)
+      fetch("./validate-login-code", {
+        method: "POST",
+        body: JSON.stringify({code: response.code}),
+        headers: {
+          "Content-Type": "application/json",
+          "X-Requested-With": "javascript-fetch"
         }
       })
       .then(response => response.json())
       .then(data => console.log(data))
+      // const xhr = new XMLHttpRequest();
+      // xhr.open('POST', code_receiver_uri, true);
+      // xhr.onload = function() {
+      //   console.log('Auth code response: ' + xhr.responseText);
+      // };
+      // xhr.send('code=' + code);
     },
   });
 
-  client.requestAccessToken();
+  client.requestCode()
 }
+
+document.getElementsByClassName("sign-in")[0].children[0].addEventListener("click", signIn)

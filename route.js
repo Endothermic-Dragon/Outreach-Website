03b9a46b7f2ea443565e476146e2c01995554408
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-05-17 21:13:13 -0400

    More secure sign-in + added to do list

--- route.js
+++ route.js
@@ -1,111 +1,144 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
+const peopleAPI = google.people({
+  version: "v1",
+});
+const uuid = require("uuid").v4;
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 80;
 
-const oauth2Client = new google.auth.OAuth2(
+function newClient(){
+  return new google.auth.OAuth2(
     "672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com",
     "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",
     "http://localhost"
-);
+  );
+}
 
-const peopleAPI = google.people({
-    version: 'v1'
-})
+// Replace with database
+let token_data = {};
 
-const uuid = require('uuid').v4;
-token_data = {}
-// var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database('./user_tokens.db', (err) => {
-// if (err) {
-//     console.error(err.message);
-// }
-// console.log('Connected to the my database.');
-// });
+// Database format
+// Col 1: cookie ID
+// Col 2: tokens required to get data
+// Col 3: google ID
+// When adding new data, wipe old data with same google ID
+const sqlite3 = require("sqlite3").verbose();
+const userTokensDB = new sqlite3.Database("./user_tokens.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the my database.");
+});
 
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const app = express()
-const port = 80
-var queue = []
-var running = false
+// Get profile details from ID token
+async function getUserDetails(oauth2Client, tokens) {
+  oauth2Client.setCredentials(tokens);
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+  let response = await peopleAPI.people.get({
+    resourceName: "people/me",
+    personFields: "names,photos,emailAddresses",
+    auth: oauth2Client,
+  });
 
-async function getUserDetails(tokens){
-    while (running){
-        await sleep(100)
-    }
-    running = true
+  let publicData = {
+    name: response.data.names.find((el) => el.metadata.primary).displayName,
+    photo: response.data.photos.find((el) => el.metadata.primary).url,
+  };
 
-    oauth2Client.setCredentials(tokens)
+  let privateData = {
+    email: response.data.emailAddresses.find((el) => el.metadata.primary).value,
+    googleID: response.data.resourceName.slice(7),
+  };
 
-    let response = await peopleAPI.people.get({
-        resourceName: "people/me",
-        personFields: "names,photos,emailAddresses",
-        auth: oauth2Client
-    })
-
-    let publicData = {
-        name: response.data.names.find(el => el.metadata.primary).displayName,
-        photo: response.data.photos.find(el => el.metadata.primary).url
-    }
-
-    let privateData = {
-        email: response.data.emailAddresses.find(el => el.metadata.primary).value,
-        googleID: response.data.resourceName.slice(7)
-    }
-
-    running = false
-    return [publicData, privateData]
+  return [publicData, privateData];
 }
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/auto-login-user", async function(req, res){
-    if (token_data[req.cookies.userID]){
-        let userData = await getUserDetails(JSON.parse(token_data[req.cookies.userID])[0])
-        res.status(200).send({data: userData[0]})
-        // Handle if error
+// Serve authentication-related URLs
+
+app.get("/auto-login-user", async function (req, res) {
+  // TO DO: Validate origin URL
+
+  if (req.get("X-Requested-With") == "javascript-fetch") {
+    if (token_data[req.cookies.userID]) {
+      // Initialize client
+      const oauth2Client = newClient()
+
+      // Handle if error (500)
+      let userData = await getUserDetails(
+        oauth2Client,
+        JSON.parse(token_data[req.cookies.userID])[0]
+      );
+      res.status(200).send({ data: userData[0] });
     } else {
-        res.status(404).send()
+      // Not in token database
+      res.status(400).send();
     }
-})
-
-app.post("/validate-login-code", async function(req, res){
-    if (req.get("X-Requested-With") == "javascript-fetch"){
-        let tokenResponse = await oauth2Client.getToken(req.body.code)
-
-        let userID = uuid()
-        while (true){
-            if (!token_data[userID]){
-                break
-            }
-            userID = uuid()
-        }
-
-        let [publicData, privateData] = await getUserDetails(tokenResponse.tokens)
-
-        token_data[userID] = JSON.stringify([tokenResponse.tokens, privateData])
-
-        console.log(publicData)
-        res.cookie("userID", userID)
-        res.status(200).send({data: publicData})
-    }
-})
-
-app.use("/static", express.static('build/static'))
-app.use("/style", express.static('build/style'))
-app.use("/scripts", express.static('build/scripts'))
-
-app.use('/', function(req, res, next){
-    if (req.url.slice(-5) == ".html") {
-        res.status(200);
-        return res.redirect(req.url.slice(0,-5));
-    }
-    next()
+  } else {
+    res.status(404).send();
+  }
 });
 
-app.use(function(req, res){
-    res.sendFile(req.url + ".html", { root: __dirname + "/build" })
-})
+app.post("/validate-login-code", async function (req, res) {
+  // Initialize client
+  const oauth2Client = newClient()
+
+  // TO DO: Validate origin URL
+
+  if (req.get("X-Requested-With") == "javascript-fetch") {
+    // Handle if error (500)
+    let tokenResponse = await oauth2Client.getToken(req.body.code);
+
+    // Generate UNIQUE value to ID user
+    let cookieID = uuid();
+    while (true) {
+      if (!token_data[cookieID]) {
+        break;
+      }
+      cookieID = uuid();
+    }
+
+    let [publicData, privateData] = await getUserDetails(oauth2Client, tokenResponse.tokens);
+    if (
+      ["htps.us", "gmail.com"].includes(
+        privateData.email.split("@").slice(-1)[0]
+      )
+    ) {
+      // Wipe old data with same google ID
+      token_data[cookieID] = JSON.stringify([
+        tokenResponse.tokens,
+        privateData.googleID,
+      ]);
+
+      res.cookie("userID", cookieID);
+      res.status(200).send({ data: publicData });
+    }
+  } else {
+    // Unauthorized request
+    res.status(404).send();
+  }
+});
+
+// Serve regular files
+
+app.use("/static", express.static("build/static"));
+app.use("/style", express.static("build/style"));
+app.use("/scripts", express.static("build/scripts"));
+
+app.use("/", function (req, res, next) {
+  if (req.url.slice(-5) == ".html") {
+    res.status(200);
+    return res.redirect(req.url.slice(0, -5));
+  }
+  next();
+});
+
+app.use(function (req, res) {
+  res.sendFile(req.url + ".html", { root: __dirname + "/build" });
+});
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
--- src/static/google-sign-in.js
+++ src/static/google-sign-in.js
@@ -1,23 +1,4 @@
 function signIn(){
-  // const client = google.accounts.oauth2.initTokenClient({
-  //   client_id: '672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com',
-  //   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
-  //   callback: (tokenResponse) => {
-  //     console.log(tokenResponse)
-      
-  //     fetch('https://people.googleapis.com/v1/people/me?personFields=names,photos,emailAddresses', {
-  //       "method": "GET",
-  //       "headers": {
-  //         "Authorization": "Bearer " + tokenResponse.access_token
-  //       }
-  //     })
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  //   },
-  // });
-
-  // client.requestAccessToken();
-
   const client = google.accounts.oauth2.initCodeClient({
     client_id: '672955273389-bc25j23ds73qgp7ukroaloutv2a22qjv.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
@@ -40,14 +21,24 @@ function signIn(){
   client.requestCode()
 }
 
-fetch("./auto-login-user")
+// Send custom headers for authentication
+fetch("./auto-login-user", {
+  headers: {
+    "Content-Type": "application/json",
+    "X-Requested-With": "javascript-fetch"
+  }
+})
 .then(async res => {
-  if (res.status != 200){
+  if (res.status == 400){
+    // Display log in button
     let button = document.getElementsByClassName("sign-in")[0]
     button.classList.add("show")
     button.children[0].addEventListener("click", signIn)
+  } else if (res.status == 200) {
+    // Display profile data
+    let data = await res.json()
+    console.log(data)
   } else {
-    // Display profile
-    console.log(await res.json())
+    // Other error
   }
 })

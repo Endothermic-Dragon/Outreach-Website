bd9e859eaed04f70520856cc5d46f3ccde24f602
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-05-19 16:44:15 -0400

    Figuring out database?

---
+++ test.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+// client.query("CREATE DATABASE test", (err, res) => {
+//   if (err)
+// })
+// client.end()
+client.query('drop table test;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
